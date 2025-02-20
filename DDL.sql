8a5f7a315110fb661430f2c5fbe11a7ae364d59d
Alexander Addis <addisa@classwork.engr.oregonstate.edu>
2025-02-19 19:55:23 -0800

    changed html based on feedback

--- DDL.sql
+++ DDL.sql
@@ -46,7 +46,7 @@ CREATE TABLE Sales (
   payment DECIMAL(11, 2) NOT NULL,
   date DATETIME NOT NULL,
   Customers_idCustomer INT NOT NULL,
-  Employees_idEmployee INT NOT NULL,
+  Employees_idEmployee INT,
   FOREIGN KEY (Customers_idCustomer) 
     REFERENCES Customers(idCustomer)
     ON DELETE RESTRICT,
@@ -151,7 +151,9 @@ INSERT INTO Manufacturers (name, email, phone, address)
 VALUES 
 ('Gucci', 'gucci@gmail.com', '(541)555-1948', '91-487 Somewhere Pl.'),
 ('Versace', 'versace@gmail.com', '(541)555-9876', '435 Road St.'),
-('Prada', 'prada@gmail.com', '(541)555-8762', '9002 Boulevard Ave.');
+('Prada', 'prada@gmail.com', '(541)555-8762', '9002 Boulevard Ave.'),
+('Nike', 'nike@gmail.com', '(541)555-3827', '123 Place Rd.'),
+('Uniqlo', 'uniqlo@gmail.com', '(541)555-2911', '98-324 Sidewalk Ave.');
 
 -- --------------------------------------------------
 -- Insert sample data into Clothes table
@@ -183,6 +185,24 @@ VALUES
   21.99, 
   20.89, 
   1, 
+  (SELECT idManufacturer FROM Manufacturers WHERE name = 'Prada')
+),
+(
+  'Hat',
+  'Accessories',
+  'Large',
+  7.99,
+  7.99,
+  1,
+  (SELECT idManufacturer FROM Manufacturers WHERE name = 'Gucci')
+),
+(
+  'Shorts',
+  'Bottoms',
+  'Medium',
+  10.99,
+  10.99,
+  1,
   (SELECT idManufacturer FROM Manufacturers WHERE name = 'Prada')
 );
 
