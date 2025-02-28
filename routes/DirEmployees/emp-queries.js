d10ab65bcc945b4f5e0f6d9b1d8e791ffa36225f
Alexander Addis <addisa@classwork.engr.oregonstate.edu>
2025-02-27 20:17:08 -0800

    added queries to emp-queries.js

--- routes/DirEmployees/emp-queries.js
+++ routes/DirEmployees/emp-queries.js
@@ -1,8 +1,8 @@
 let queries = {
   read: "SELECT idEmployee, firstName AS 'First Name', lastName AS 'Last Name', hireDate AS 'Hire Date', email, phone FROM Employees;",
-  create: "",
-  update: "",
-  delete: "",
+  create: "INSERT INTO Employees (firstName, lastName, hireDate, email, phone) VALUES (:givenFirstName, :givenLastName, :givenHireDate, :givenEmail, :givenPhone);",
+  update: "UPDATE Employees SET firstName = :givenFirstName, lastName = :givenLastName, hireDate = :givenHireDate, email = :givenEmail, phone = :givenPhone WHERE idEmployee = :givenIdFromForm;",
+  delete: "DELETE FROM Employees WHERE idEmployee = :givenIdFromForm;",
 }
 
 export default queries;
