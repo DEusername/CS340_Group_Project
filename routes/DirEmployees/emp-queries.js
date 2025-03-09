49c32297550ead4b5c0721b0ffd9a05a919e6598
Duncan Everson <duncan.everson@icloud.com>
2025-03-08 21:52:18 -0800

    added a trailing ; to emp-routes

--- routes/DirEmployees/emp-queries.js
+++ routes/DirEmployees/emp-queries.js
@@ -2,7 +2,7 @@ let queries = {
   read: "SELECT idEmployee, firstName AS 'First Name', lastName AS 'Last Name', hireDate AS 'Hire Date', email, phone FROM Employees;",
   create: "INSERT INTO Employees (firstName, lastName, hireDate, email, phone) VALUES (?, ?, ?, ?, ?);",
   update: "UPDATE Employees SET firstName = ?, lastName = ?, hireDate = ?, email = ?, phone = ? WHERE idEmployee = ?;",
-  delete: "DELETE FROM Employees WHERE idEmployee = ? "
+  delete: "DELETE FROM Employees WHERE idEmployee = ?;"
 }
 
 export default queries;
