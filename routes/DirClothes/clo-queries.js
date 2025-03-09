e3587cf3063f86bd83c249495123f166a3e518e5
Duncan Everson <duncan.everson@icloud.com>
2025-03-09 03:15:05 -0700

    fixed bugs with clothes ejs page and routing

--- routes/DirClothes/clo-queries.js
+++ routes/DirClothes/clo-queries.js
@@ -1,8 +1,9 @@
 let queries = {
-  read: "SELECT Clothes.name, Clothes.category, Clothes.size, Clothes.price, Clothes.purchaseCost AS 'Purchase Cost', Clothes.stock, Manufacturers.name AS 'Manufacturer' FROM Clothes INNER JOIN Manufacturers ON Clothes.Manufacturers_idManufacturer = Manufacturers.idManufacturer;",
-  create: "INSERT INTO Clothes (name, category, size, price, purchaseCost, stock, Manufacturers_idManufacturer) VALUES (?, ?, ?, ?, ?, ?, (SELECT idManufacturer FROM Manufacturer WHERE name = ?));",
+  read: "SELECT Clothes.idClothes, Clothes.name, Clothes.category, Clothes.size, Clothes.price, Clothes.purchaseCost AS 'Purchase Cost', Clothes.stock, Manufacturers.name AS 'Manufacturer' FROM Clothes INNER JOIN Manufacturers ON Clothes.Manufacturers_idManufacturer = Manufacturers.idManufacturer;",
+  create: "INSERT INTO Clothes (name, category, size, price, purchaseCost, stock, Manufacturers_idManufacturer) VALUES (?, ?, ?, ?, ?, ?, (SELECT idManufacturer FROM Manufacturers WHERE name = ?));",
   update: "UPDATE Clothes SET name = ?, category = ?, size = ?, price = ?, purhcaseCost = ?, stock = ?, Manufacturers_idManufacturer = (SELECT idManufacturer FROM Manufacturer WHERE name = ?) WHERE idClothes = ?;",
-  delete: "DELETE FROM Clothes WHERE idClothes = ?;"
+  delete: "DELETE FROM Clothes WHERE idClothes = ?;",
+  getManu: "SELECT name FROM Manufacturers;"
 }
 
 export default queries;
--- routes/DirClothes/cloRouter.js
+++ routes/DirClothes/cloRouter.js
@@ -7,38 +7,45 @@ const cloRouter = express.Router();
 
 // clothes page
 cloRouter.get('/', async (req, res) => {
-    // read most current data from the db
-    let [results, fields] = await db.query(queries.read);
+  // read most current data from the db
+  let [clothesResults, clothesFields] = await db.query(queries.read);
+  let [manuResults, manuFields] = await db.query(queries.getManu);
 
-    // serve the clothes webpage back to the user
-    res.render('clothes', { records: results })
+  // serve the clothes webpage back to the user
+  res.render('clothes', { records: clothesResults, manuNames: manuResults })
 });
 
 // create clothes entry
 cloRouter.post('/create', async (req, res) => {
-    let [results, fields] = await db.query(queries.create, [req.body.name, req.body.category, req.body.size, req.body.price, req.body.purchaceCost, req.body.stock, req.body.Manufacturers_idManufacturer]);
+  if (req.body.size !== 'non-value' && req.body.manufacturer !== 'non-value') {
+    let [results, fields] = await db.query(queries.create, [req.body.name, req.body.category, req.body.size, req.body.price, req.body.purchaseCost, req.body.stock, req.body.manufacturer]);
     if (results.affectedRows != 1)
-        console.log("create failed");
+      console.log("create failed");
+  }
 
-    res.redirect('/clothes');
+  res.redirect('/clothes');
 });
 
 // update clothes entry data
 cloRouter.post('/update', async (req, res) => {
-    let [results, fields] = await db.query(queries.create, [req.body.name, req.body.category, req.body.size, req.body.price, req.body.purchaceCost, req.body.stock, req.body.Manufacturers_idManufacturer, req.body.ID]);
+  if (req.body.ID !== '0' && req.body.size !== 'non-value' && req.body.manufacturer !== 'non-value') {
+    let [results, fields] = await db.query(queries.update, [req.body.name, req.body.category, req.body.size, req.body.price, req.body.purchaseCost, req.body.stock, req.body.manufacturer, req.body.ID]);
     if (results.affectedRows != 1)
-        console.log("update failed");
+      console.log("update failed");
+  }
 
-    res.redirect('/clothes');
+  res.redirect('/clothes');
 });
 
 // delete clothes entry
 cloRouter.post('/delete', async (req, res) => {
+  if (req.body.ID !== 'non-value') {
     let [results, fields] = await db.query(queries.delete, [req.body.ID]);
     if (results.affectedRows != 1)
-        console.log("delete failed");
+      console.log("delete failed");
+  }
 
-    res.redirect('/clothes');
+  res.redirect('/clothes');
 });
 
 export default cloRouter;
