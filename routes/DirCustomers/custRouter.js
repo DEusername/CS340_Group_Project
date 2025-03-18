8c869e519559fb36c2ef418d271a582144049e49
Duncan Everson <duncan.everson@icloud.com>
2025-03-17 20:22:29 -0700

    made customer and employee views handle duplicate entry errors gracefully.

--- routes/DirCustomers/custRouter.js
+++ routes/DirCustomers/custRouter.js
@@ -9,6 +9,7 @@ const custRouter = express.Router();
 custRouter.get('/', async (req, res) => {
   // read most current data from the db
   let [results, fields] = await db.query(queries.read);
+  console.log(results);
 
   // serve the customers webpage back to the user
   res.render('customers', { records: results })
@@ -16,26 +17,51 @@ custRouter.get('/', async (req, res) => {
 
 // create customer entry
 custRouter.post('/create', async (req, res) => {
-  const email = req.body.email === "" ? null : req.body.email
-  const phone = req.body.phone === "" ? null : req.body.phone
-  let [results, fields] = await db.query(queries.create, [req.body.firstName, req.body.lastName, email, phone]);
-  if (results.affectedRows != 1)
-    console.log("create failed");
-
-  res.redirect('/customers');
+  try {
+    const email = req.body.email === "" ? null : req.body.email
+    const phone = req.body.phone === "" ? null : req.body.phone
+    let [results, fields] = await db.query(queries.create, [req.body.firstName, req.body.lastName, email, phone]);
+    if (results.affectedRows != 1)
+      console.log("create failed");
+    res.redirect('/customers');
+    return
+  } catch (error) {
+    let errMessage;
+    if (error.code === 'ER_DUP_ENTRY') {
+      errMessage = `You attempted to enter either a duplicate email or phone number.
+      Please ensure that the email or phone number you are attempting to use to make
+      the customer record is unique to other existing emails or phone numbers`;
+    }
+    else
+      errMessage = `An unknown error occured with the creation of the record.`;
+    res.render('customers', { message: errMessage })
+    return
+  }
 });
 
 // update customer entry data
 custRouter.post('/update', async (req, res) => {
   if (req.body.ID !== '0') {
-    const email = req.body.email === "" ? null : req.body.email
-    const phone = req.body.phone === "" ? null : req.body.phone
-    let [results, fields] = await db.query(queries.update, [req.body.firstName, req.body.lastName, email, phone, req.body.ID]);
-    if (results.affectedRows != 1)
-      console.log("update failed")
-
-    res.redirect('/customers');
-    return
+    try {
+      const email = req.body.email === "" ? null : req.body.email
+      const phone = req.body.phone === "" ? null : req.body.phone
+      let [results, fields] = await db.query(queries.update, [req.body.firstName, req.body.lastName, email, phone, req.body.ID]);
+      if (results.affectedRows != 1)
+        console.log("update failed")
+      res.redirect('/customers');
+      return
+    } catch (error) {
+      let errMessage;
+      if (error.code === 'ER_DUP_ENTRY') {
+        errMessage = `You attempted to enter either a duplicate email or phone number.
+      Please ensure that the email or phone number you are attempting to use to update
+      the customer record is unique to other existing emails or phone numbers`;
+      }
+      else
+        errMessage = `An unknown error occured with the creation of the record.`;
+      res.render('customers', { message: errMessage })
+      return
+    }
   }
   else {
     let nonValMessage;
--- routes/DirEmployees/empRouter.js
+++ routes/DirEmployees/empRouter.js
@@ -26,24 +26,50 @@ empRouter.get('/', async (req, res) => {
 
 // create employee entry
 empRouter.post('/create', async (req, res) => {
-  const phone = req.body.phone === "" ? null : req.body.phone
-  let [results, fields] = await db.query(queries.create, [req.body.firstName, req.body.lastName, req.body.hireDate, req.body.email, phone]);
-  if (results.affectedRows != 1)
-    console.log("create failed");
-
-  res.redirect('/employees');
+  try {
+    const phone = req.body.phone === "" ? null : req.body.phone
+    let [results, fields] = await db.query(queries.create, [req.body.firstName, req.body.lastName, req.body.hireDate, req.body.email, phone]);
+    if (results.affectedRows != 1)
+      console.log("create failed");
+    res.redirect('/employees');
+    return
+  } catch (error) {
+    let errMessage;
+    if (error.code === 'ER_DUP_ENTRY') {
+      errMessage = `You attempted to enter either a duplicate email or phone number.
+      Please ensure that the email or phone number you are attempting to use to make
+      the employee record is unique to other existing emails or phone numbers`;
+    }
+    else
+      errMessage = `An unknown error occured with the creation of the record.`;
+    res.render('employees', { message: errMessage })
+    return
+  }
 });
 
 // update employee entry data
 empRouter.post('/update', async (req, res) => {
   if (req.body.ID !== '0') {
-    const phone = req.body.phone === "" ? null : req.body.phone
-    let [results, fields] = await db.query(queries.update, [req.body.firstName, req.body.lastName, req.body.hireDate, req.body.email, phone, req.body.ID]);
-    if (results.affectedRows != 1)
-      console.log("update failed")
+    try {
+      const phone = req.body.phone === "" ? null : req.body.phone
+      let [results, fields] = await db.query(queries.update, [req.body.firstName, req.body.lastName, req.body.hireDate, req.body.email, phone, req.body.ID]);
+      if (results.affectedRows != 1)
+        console.log("update failed")
 
-    res.redirect('/employees');
-    return
+      res.redirect('/employees');
+      return
+    } catch (error) {
+      let errMessage;
+      if (error.code === 'ER_DUP_ENTRY') {
+        errMessage = `You attempted to enter either a duplicate email or phone number.
+      Please ensure that the email or phone number you are attempting to use to update
+      the employee record is unique to other existing emails or phone numbers`;
+      }
+      else
+        errMessage = `An unknown error occured with the creation of the record.`;
+      res.render('employees', { message: errMessage })
+      return
+    }
   }
   else {
     let nonValMessage;
